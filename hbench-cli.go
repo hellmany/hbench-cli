21f8c2ecda4e112c76b174e3079c62f9fae62c1f
hellman <hellmanh@gmail.com>
2022-09-11 20:43:06 +0300

    help fix

--- hbench-cli.go
+++ hbench-cli.go
@@ -11,11 +11,11 @@ func main() {
 	c := hbench.ConfData{}
 
 	var action string
-	flag.StringVar(&action, "a", "", "Action gen|bench")
+	flag.StringVar(&action, "a", "", "Action gen|bench - required")
 
-	flag.IntVar(&c.Threads, "t", 100, "threads to write")
-	flag.IntVar(&c.Max, "m", 1000, "max files, 0 - to read all")
-	flag.IntVar(&c.Inter, "i", 1, "interations")
+	flag.IntVar(&c.Threads, "t", 100, "threads to read/write")
+	flag.IntVar(&c.Max, "m", 1000, "max files to read/wrie, 0 - to read all")
+	flag.IntVar(&c.Inter, "i", 1, "interations to read")
 	flag.IntVar(&c.Size, "s", 1, "size to read/write in Mb, 0 - full file to read")
 	flag.IntVar(&c.RandSize, "r", 3, "add rand mb to size writing")
 	flag.StringVar(&c.Path, "p", "/stor/tmp", "path")
