cfd699d4e3b4ba2fb9b33d2c9fe87ef2185d59ba
hellman <hellmanh@gmail.com>
2022-09-13 20:07:43 +0300

    fix

--- hbench-cli.go
+++ hbench-cli.go
@@ -14,7 +14,8 @@ func main() {
 	flag.StringVar(&action, "a", "", "Action gen|bench - required")
 
 	flag.IntVar(&c.Threads, "t", 100, "threads to read/write")
-	flag.IntVar(&c.Max, "m", 1000, "max files to read/wrie, 0 - to read all")
+	flag.IntVar(&c.Max, "m", 1000, "max files to read/write, 0 - to read all")
+	flag.IntVar(&c.LimitMax, "l", 1000, "limit max files to read before rand, 0 - to read all")
 	flag.IntVar(&c.Inter, "i", 1, "interations to read")
 	flag.IntVar(&c.Size, "s", 1, "size to read/write in Mb, 0 - full file to read")
 	flag.IntVar(&c.RandSize, "r", 3, "add rand mb to size writing")
